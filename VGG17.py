1757796b88c6493103a0f201dbeb976635c3627f
IChitandaI <2412012958@qq.com>
2021-02-04 22:27:10 +0800

    commit

--- VGG17.py
+++ VGG17.py
@@ -1,20 +1,23 @@
 import torch
 import torch.nn as nn
 import torchvision.models as models
+from torchvision import transforms
 
 class VGG19(nn.Module):
     def __init__(self, output_index=26):
+        super().__init__()
         self.vgg=models.vgg19(pretrained=True)
         self.features=self.vgg.features
-
-    #def process(self, x):
-
+        self.output_index=output_index
+    def process(self, x):
+        normalize=transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+        x=normalize(x)
     def forward(self, x):
-        x=self.process(x)
         x=self.features[:self.output_index](x)
         return x
 
 if __name__=="__main__":
-    x=torch.rand((64, 64), dtype=float32)
+    x=torch.rand((1, 3, 224, 224), dtype=float32)
     net=VGG19()
     x=net(x)
